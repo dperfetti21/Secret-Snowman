4cc57f10cba32884f0066dc72ee71bda40ae80cd
Declinda <dperfetti21@scarsdaleschools.org>
2019-12-02 15:05:42 -0500

    Initial sharing of project

---
+++ SecretSnowman.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class SecretSnowman here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+
+public class SecretSnowman
+{
+    public static void main (String[] args)
+    {
+     ArrayList<String>Buyer= new ArrayList<String>();
+     ArrayList<String>Giver= new ArrayList<String>();
+    } 
+    public static void readInput() 
+     {
+        Scanner myFile = new Scanner (new File("names.txt"));
+
+    
+     
+    }   
+}
+
+
